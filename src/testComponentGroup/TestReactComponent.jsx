0c0ee12085f6cd99b3fcc31493619e615bd6dbf9
Greg <gregmichaeldavies@gmail.com>
2020-11-19 20:52:17 +0000

    Test component

---
+++ src/testComponentGroup/TestReactComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+export default class TestReactComponent extends Component {
+  render() {
+    return (
+      <>
+        Test React Component is... operational!
+      </>
+    );
+  }
+}
