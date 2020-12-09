63fba46792426e0c0a3e9477e1eb048b1231bba6
Greg <gregmichaeldavies@gmail.com>
2020-12-09 20:14:24 +0000

    Feature/6 jump cursor between inputs (#12)
    * #6 Updated service to include a setFocus function.
    
    * Implemented key-triggered text inputs.

--- src/delimitedTextInput/DelimitedTextInput.jsx
+++ src/delimitedTextInput/DelimitedTextInput.jsx
@@ -34,6 +34,13 @@ export default class DelimitedTextInput extends Component {
       delimiter: '-'
     };
   }
+  jump(idx) {
+    const next = this.childInputRefs[idx];
+    if (next) {
+      next.select();
+    }
+    this.setState({ readyToJump: false });
+  }
   componentDidMount() {
     this.props.onUpdate(this.service, 'initialise');
   }
@@ -41,11 +48,56 @@ export default class DelimitedTextInput extends Component {
     this.service.setItem(value, idx);
     this.props.onUpdate(this.service, 'change');
   }
+  handleKeyDown(idx) {
+    const target = this.childInputRefs[idx];
+    const selection = target.selectionStart !== target.selectionEnd;
+    this.setState({ selection });
+  }
+  handleKeyUp(e, idx) {
+    const readyToJump = this.state.readyToJump;
+    const target = this.childInputRefs[idx];
+    if (readyToJump) {
+      if (readyToJump === 'left' && e.code === "ArrowLeft") {
+        this.jump(idx - 1);
+      } else if (readyToJump === 'right' && e.code === "ArrowRight") {
+        this.jump(idx + 1);
+      }
+    } else {
+      if (!this.state.selection) {
+        // If the current cursor position is not a selection.
+        if (e.code === "ArrowLeft" && target.selectionStart === 0 && idx - 1 >= 0) {
+          // If the left arrow has been pressed
+          // And the cursor position is 0
+          // And the next text field to the left exists
+          this.setState({ readyToJump: 'left' });
+        } else if (e.code === "ArrowRight" && target.selectionStart === target.value.length && idx + 1 < this.service.getItems().length) {
+          // If the right arrow has been pressed
+          // And the cursor position is the length of the contents
+          // And the next text field to the right exists
+          this.setState({ readyToJump: 'right' });
+        }
+      }
+    }
+  }
+  handleInputReference(inputElement) {
+    if (inputElement) {
+      this.childInputRefs.push(inputElement);
+    }
+  }
   renderItem(item, idx) {
     return (
       <>
         { idx === 0 ? '' : this.renderDelimiter() }
-        <Input className="delimitedTextInput__input" value={item.value} style={{ width: `${item.width}px` }} onChange={e => this.handleChange(e.target.value, idx)} />
+        <Input
+          className="delimitedTextInput__input"
+          type="text"
+          value={item.value}
+          style={{ width: `${item.width}px` }}
+          onChange={e => this.handleChange(e.target.value, idx)}
+          onKeyUp={e => this.handleKeyUp(e, idx)}
+          onKeyDown={() => this.handleKeyDown(idx)}
+          ref={this.handleInputReference.bind(this)}
+        />
       </>
     );
   }
@@ -53,6 +105,7 @@ export default class DelimitedTextInput extends Component {
     return (<span className="delimitedTextInput__delimiter">{this.state.delimiter}</span>);
   }
   render() {
+    this.childInputRefs = [];
     return (
       <Container className="delimitedTextInput__container">
         {
