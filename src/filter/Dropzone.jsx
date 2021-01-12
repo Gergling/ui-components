494703891498ee3074067d56c5b143f642c36510
Greg <gregmichaeldavies@gmail.com>
2021-01-12 22:21:32 +0000

    #13 Styling improvements.

--- src/filter/Dropzone.jsx
+++ src/filter/Dropzone.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { useDrop } from 'react-dnd';
 
 const style = {
-  height: '12rem',
-  width: '12rem',
-  marginRight: '1.5rem',
-  marginBottom: '1.5rem',
-  color: 'white',
+  color: 'darkgrey',
   padding: '1rem',
   textAlign: 'center',
   fontSize: '1rem',
   lineHeight: 'normal',
-  float: 'left',
+  border: 'dotted 3px',
 };
 function selectBackgroundColor(isActive, canDrop) {
   if (isActive) {
@@ -21,26 +17,24 @@ function selectBackgroundColor(isActive, canDrop) {
       return 'darkkhaki';
   }
   else {
-      return 'grey';
+      return 'lightgrey';
   }
 }
-// TODO: Accept a "single" or "multi" mode depending on the number of children that should go into the model.
-// Needs some form of model management, including a getter and setter of some kind.
 export const Dropzone = ({ children, name, onDrop }) => {
   const [{ canDrop, isOver, isOverCurrent }, drop] = useDrop({
-      accept: 'any',
+    accept: 'any',
     drop: item => {
       if (isOverCurrent) {onDrop(item, name);}
-        return {
-          name,
-          allowedDropEffect: 'any',
-        }
-      },
-      collect: (monitor) => ({
-          isOver: monitor.isOver(),
-          canDrop: monitor.canDrop(),
-        isOverCurrent: monitor.isOver({ shallow: true }),
-      }),
+      return {
+        name,
+        allowedDropEffect: 'any',
+      }
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+      isOverCurrent: monitor.isOver({ shallow: true }),
+    }),
   });
   const isActive = canDrop && isOver && isOverCurrent;
   const backgroundColor = selectBackgroundColor(isActive, canDrop);
