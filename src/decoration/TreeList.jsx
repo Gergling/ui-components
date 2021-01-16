fc8acd8040ea738350d90117c3c80743cc81cfea
Greg <gregmichaeldavies@gmail.com>
2021-01-16 21:41:07 +0000

    #13 Implemented some TreeList parameters.

--- src/decoration/TreeList.jsx
+++ src/decoration/TreeList.jsx
@@ -15,50 +15,75 @@ const VerticalSpacer = styled.div`
   height: 50%;
 `;
 const VerticalBar = styled(VerticalSpacer)`
-  border-left: solid 1px black;
+  border-style: solid;
+  border-width: 0;
+  border-left-width: ${props => props.thickness};
+  border-color: ${props => props.colour};
   width: 0;
   height: 50%;
 `;
+VerticalBar.defaultProps = { colour: 'black', thickness: '1px' };
+
 const VerticalCorner = styled(VerticalBar)`
+  /* height: ${props => props.thickness}; */
   height: 1px;
 `;
 
-const HorizontalContainer = () => (
+const HorizontalContainer = ({ width, colour, thickness }) => (
   <div style={{ display: 'flex', 'flex-direction': 'column' }}>
-    <HorizontalSpacer/>
-    <HorizontalBar/>
+    <HorizontalSpacer width={width} />
+    <HorizontalBar width={width} colour={colour} thickness={thickness} />
   </div>
 );
 const HorizontalSpacer = styled.div`
-  width: 5px;
+  width: ${props => props.width};
   flex-grow: 1;
 `;
+HorizontalSpacer.defaultProps = { width: '5px' };
+
 const HorizontalBar = styled(HorizontalSpacer)`
-  border-top: solid 1px black;
+  border-style: solid;
+  border-width: 0;
+  border-top-width: ${props => props.thickness};
+  border-color: ${props => props.colour};
 `;
+HorizontalBar.defaultProps = { colour: 'black', thickness: '1px' };
 
 class Brace extends Component {
+  renderVerticalBar() {
+    return <VerticalBar colour={this.props.colour} thickness={this.props.thickness} />;
+  }
   render() {
     return (
       <ColumnContainer>
         <div>
-          {this.props.top ? <VerticalSpacer/> : <VerticalBar/>}
-          {this.props.bottom ? <VerticalCorner/> : <VerticalBar/>}
+          {this.props.top ? <VerticalSpacer/> : this.renderVerticalBar()}
+          {this.props.bottom ? <VerticalCorner colour={this.props.colour} thickness={this.props.thickness} /> : this.renderVerticalBar()}
         </div>
-        <HorizontalContainer />
+        <HorizontalContainer colour={this.props.colour} width={this.props.width} thickness={this.props.thickness} />
       </ColumnContainer>
     );
   }
 }
 
+// TODO: Needs props implemented.
 export default class TreeList extends Component {
+  getHorizontalSpacerWidth() {
+    return this.props.spacing;
+  }
+  getColour() {
+    return this.props.colour;
+  }
+  getThickness() {
+    return this.props.thickness;
+  }
   renderChild(child, key, children) {
     const top = key === 0;
     const bottom = key === children.length - 1;
     return (
       <ListItem key={key}>
         <ColumnContainer>
-          <Brace top={top} bottom={bottom} />
+          <Brace top={top} bottom={bottom} colour={this.getColour()} thickness={this.getThickness()} />
           <div>{child}</div>
         </ColumnContainer>
       </ListItem>
@@ -67,9 +92,9 @@ export default class TreeList extends Component {
   render() {
     return (
       <ColumnContainer>
-        <HorizontalContainer />
+        <HorizontalContainer colour={this.getColour()} width={this.getHorizontalSpacerWidth()} thickness={this.getThickness()} />
         <ListContainer>
-          {this.props.children.map(this.renderChild)}
+          {this.props.children.map(this.renderChild.bind(this))}
         </ListContainer>
       </ColumnContainer>
     );
--- stories/decoration.stories.js
+++ stories/decoration.stories.js
@@ -10,4 +10,28 @@ storiesOf('TreeList', module)
     <div>B</div>
     <div>C</div>
     <div>D</div>
+  </TreeList>))
+  .add('with four irregular children', () => (<TreeList>
+    <div>A if it was a bit longer</div>
+    <div>B, but also<br/>E</div>
+    <div>C</div>
+    <div>D</div>
+  </TreeList>))
+  .add('with four children in a red tree', () => (<TreeList colour="#f00">
+    <div>A</div>
+    <div>B</div>
+    <div>C</div>
+    <div>D</div>
+  </TreeList>))
+  .add('with four children and more space', () => (<TreeList spacing="10px">
+    <div>A</div>
+    <div>B</div>
+    <div>C</div>
+    <div>D</div>
+  </TreeList>))
+  .add('with four children and thicker lines', () => (<TreeList thickness="3px">
+    <div>A</div>
+    <div>B</div>
+    <div>C</div>
+    <div>D</div>
   </TreeList>));
