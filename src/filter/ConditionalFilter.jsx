9e136cf2ddf421e920cc34b9291ff80bf435efcd
Greg <gregmichaeldavies@gmail.com>
2021-01-23 15:01:07 +0000

    #13 Moved dropzones and tied state into componentDidUpdate in a way that's up to date.

--- src/filter/ConditionalFilter.jsx
+++ src/filter/ConditionalFilter.jsx
@@ -12,7 +12,6 @@ import { DraggableItem } from './DraggableItem';
 
 // Style the child conditions
 
-// const ListContainer = styled.ul``;
 const ListItem = styled.div`
   padding-bottom: ${props => props.spacing};
 `;
@@ -26,11 +25,15 @@ export default class ConditionalFilter extends Component {
   getToggledOperand() {
     return this.state && this.state.operand === 'AND' ? 'OR' : 'AND';
   }
-  componentDidUpdate(props, state) {
-    props.onUpdate(state);
+  componentDidUpdate(props) {
+    // The update function is presumed to be a FilterModel.prototype.setModel function.
+    props.onUpdate(this.state);
   }
   handleOnDrop(item) {
-    this.state.childModels.push(item.onDrop());
+    const model = item.onDrop();
+    if (this.state.childModels.indexOf(model) === -1) {
+      this.state.childModels.push(model);
+    }
     this.setState({
       childModels: this.state.childModels
     });
@@ -45,11 +48,10 @@ export default class ConditionalFilter extends Component {
   handleToggleOperand() {
     this.setState({ operand: this.getToggledOperand() })
   }
-  renderChild(child, key, children) {
-    const spacing = children.length - 1 === key ? '0' : '10px';
+  renderChild(child, key) {
     // TODO: Put the deletion at the left of the control.
     return (
-      <ListItem key={key} spacing={spacing}>
+      <ListItem key={key} spacing='10px'>
         <DraggableItem onDrop={() => child}>
           {child.createUIComponent()}
           <span onClick={this.handleChildRemoval.bind(this, child)}>
@@ -59,36 +61,29 @@ export default class ConditionalFilter extends Component {
       </ListItem>
     );
   }
+  renderDropzone() {
+    return (
+      <ListItem spacing="0">
+        <Dropzone onDrop={this.handleOnDrop.bind(this)}>
+          Drag things to here.
+        </Dropzone>
+      </ListItem>
+    );
+  }
   renderChildren() {
-    return <TreeList>{this.state.childModels.map(this.renderChild.bind(this))}</TreeList>;
+    return <TreeList>
+      { this.state.childModels.map(this.renderChild.bind(this)) }
+      { this.renderDropzone() }
+    </TreeList>;
   }
   renderNonMultipleMessage() {
     return <div style={{color: 'black'}}>Drag multiple fields here to filter</div>;
-  }
-  renderDropzoneContent() {
-    // TODO: Add controls for toggling NOT and AND/OR. Probably needs a flexbox. Especially in this case since you need the AND NOT grammar.
-    // TODO: Handle logic for 0, 1 or multiple children.
-    // 0 children just indicates it's empty
-    // 1 child just highlights that the filter isn't being utilised well.
-    // Multiple children have a tree list.
-    return <>
-      <div>
-        <button onClick={this.handleToggleNot.bind(this)}>Toggle NOT</button>
-        <button onClick={this.handleToggleOperand.bind(this)}>Toggle AND/OR</button>
-      </div>
-      <div style={{ display: 'flex' }}>
-        <HorizontalBar />
-        <div style={{ display: 'flex', 'flex-direction': 'column', 'justify-content': 'center' }}><div style={{ width: '90px', padding: '5px', border: 'solid 1px black' }}>{ this.state.operand }{ this.state.not ? ' NOT' : '' }</div></div>
-        { this.state.childModels.length > 0 ? this.renderChildren() : ''}
-      </div>
-      { this.state.childModels.length < 2 ? this.renderNonMultipleMessage() : ''}
-    </>;
   }
   render() {
     // Needs the capacity to add children.
     // Each child needs to come from a list of field configurations.
     return (
-      <Dropzone onDrop={this.handleOnDrop.bind(this)}>
+      <>
         <div>
           <button onClick={this.handleToggleNot.bind(this)}>Toggle NOT</button>
           <button onClick={this.handleToggleOperand.bind(this)}>Toggle AND/OR</button>
@@ -96,10 +91,9 @@ export default class ConditionalFilter extends Component {
         <div style={{ display: 'flex' }}>
           <HorizontalBar />
           <div style={{ display: 'flex', 'flex-direction': 'column', 'justify-content': 'center' }}><div style={{ padding: '5px', border: 'solid 1px black' }}>{ this.state.operand }{ this.state.not ? ' NOT' : '' }</div></div>
-          { this.state.childModels.length > 0 ? this.renderChildren() : ''}
+          { this.renderChildren() }
         </div>
-        { this.state.childModels.length < 2 ? this.renderNonMultipleMessage() : ''}
-      </Dropzone>
+      </>
     );
   }
 }
