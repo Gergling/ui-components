9c2cb7ba49e6d9aa396c6198b01c9cc508851e6a
Greg <gregmichaeldavies@gmail.com>
2021-01-23 14:48:59 +0000

    #13 Updated to output a full structure.

--- src/filter/filterServiceFactory.js
+++ src/filter/filterServiceFactory.js
@@ -5,9 +5,21 @@ import React from 'react';
 import BetweenFilter from './BetweenFilter';
 import BooleanFilter from './BooleanFilter';
 import StringFilter from './StringFilter';
+import ConditionalFilter from './ConditionalFilter';
 
+function createElement(JSXClass, props) {
+  return React.createElement(JSXClass, props);
+}
+
+// Base class for types of filter.
 class FilterType {
-  // TODO: Decide whether this is relevant anymore, since the component will choose the structure.
+  get field() {
+    return this._field;
+  }
+  setField(field) {
+    this._field = field;
+    return this;
+  }
   getStructure() {
     throw new Error('FilterType is an abstract class and getStructure must be overridden.');
   }
@@ -18,14 +30,19 @@ class FilterType {
     this._not = not;
     return this;
   }
-  createModel() {
-    return new FilterModel(this);
+  createModel(onUpdate) {
+    return new FilterModel(this, onUpdate);
+  }
+  updateCollection(model) {
+    this.field.updateCollection(model);
   }
 }
 
+// Model object which supplies the capacity to modify the value and generate the UI Component.
 class FilterModel {
-  constructor(type) {
+  constructor(type, onUpdate) {
     this._type = type;
+    this._onUpdate = onUpdate || (() => {});
   }
   get type() {
     return this._type;
@@ -33,21 +50,21 @@ class FilterModel {
   get value() {
     return this._value;
   }
+  getStructure() {
+    return this.type.getStructure(this.value || {});
+  }
+  updateCollection() {
+    this.type.updateCollection(this);
+  }
   setValue(value) {
     this._value = value;
+    this.updateCollection();
     return this;
   }
   createUIComponent() {
-    return createElement(this.type.getUIComponentClass(), this.type.getUIComponentProperties(model => this.setValue(model.value)));
+    return createElement(this.type.getUIComponentClass(), this.type.getUIComponentProperties(this.setValue.bind(this)));
   }
 }
-
-function createElement(JSXClass, props) {
-  return React.createElement(JSXClass, props);
-}
-
-// FilterType classes
-// Instantiations of these are passed into UI components
 
 // Handles filtering between two values
 class FilterTypeBetween extends FilterType {
@@ -66,45 +83,60 @@ class FilterTypeBetween extends FilterType {
     this._end = end;
     return this;
   }
-  getStructure() {
+  getStructure(value) {
     return {
       type: 'between',
-      not: this._not,
-      start: this._start,
-      end: this._end
+      not: value.not,
+      start: value.start,
+      end: value.end
     };
   }
   getUIComponentClass() {
     return BetweenFilter;
   }
+  getUIComponentProperties(onUpdate) {
+    return {
+      mode: this.mode,
+      onUpdate
+    };
+  }
 }
 
-// Checks whether a value constitutes empty or not
+// For handling simple on/off logic.
 class FilterTypeBoolean extends FilterType {
-  getStructure() {
+  getStructure(value) {
     return {
-      type: 'empty',
-      empty: this._not
+      type: 'boolean',
+      state: value.state
     }
   }
   getUIComponentClass() {
     return BooleanFilter;
   }
+  getUIComponentProperties(onUpdate) {
+    return { onUpdate };
+  }
 }
 
-// TODO: Also needs capacity to match partial strings.
+// For matching string data.
 class FilterTypeString extends FilterType {
-  getStructure() {
+  getStructure(value) {
     return {
       type: 'string',
-      empty: this._not
+      empty: value.not,
+      start: value.start,
+      end: value.end
     }
   }
   getUIComponentClass() {
     return StringFilter;
   }
+  getUIComponentProperties(onUpdate) {
+    return { onUpdate };
+  }
 }
 
+// For nesting conditional data.
 class FilterTypeConditional extends FilterType {
   constructor() {
     super();
@@ -119,65 +151,91 @@ class FilterTypeConditional extends FilterType {
     this._operand = 'OR';
     return this;
   }
-  addChild(filterType) {
-    // Should always be passed a FilterType subclass
-    this._children.push(filterType);
-    return this;
-  }
-  getStructure() {
+  getStructure(value) {
     return {
       type: 'conditional',
-      not: this._not,
-      operand: this._operand,
-      children: this._children.map(filterType => filterType.getStructure())
+      not: value.not,
+      operand: value.operand,
+      children: (value.childModels || []).map(child => child.getStructure())
     };
   }
   getUIComponentClass() {
     return ConditionalFilter;
+  }
+  getUIComponentProperties(onUpdate) {
+    return { onUpdate };
+  }
 }
 
+// Handles field configurations.
 class Field {
-  constructor(name, label, type, filterTypes) {
+  constructor(name, label, type, filterTypes, fieldCollection) {
     this.name = name;
     this.label = label;
     this.type = type;
     this._filterTypes = filterTypes;
+    this._fieldCollection = fieldCollection;
   }
   get filterTypes() {
-    return this._filterTypes.map(filterTypeFactory => filterTypeFactory());
+    return this._filterTypes.map(filterTypeFactory => filterTypeFactory().setField(this));
+  }
+  updateCollection(model) {
+    this._fieldCollection.update(model);
   }
 }
 
+// Handles the root model and all possible field configurations.
 class FieldCollection {
   constructor() {
     this._fields = [
       new Field('conditional', 'Conditional', 'conditional', [
         () => new FilterTypeBoolean(),
         () => new FilterTypeConditional(),
-      ])
+      ], this)
     ];
+    this._onUpdate = () => {};
   }
   get fields() {
     return this._fields;
   }
+  update(model) {
+    // TODO: Indicate when the root is being deleted.
+    // Requires a separate function.
+    if (!this._root) {
+      this._root = model;
+    }
+    this._onUpdate();
+    return this;
+  }
+  get root() {
+    return this._root;
+  }
+  setUpdate(fnc) {
+    this._onUpdate = fnc;
+    return this;
+  }
   addDate(name, label) {
     this._fields.push(new Field(name, label, 'date', [
-      () => new FilterTypeBoolean(),
       () => (new FilterTypeBetween()).setMode('date'),
-    ]));
+    ], this));
     return this;
   }
   addBoolean(name, label) {
     this._fields.push(new Field(name, label, 'boolean', [
-      () => FilterTypeBoolean(),
-    ]));
+      () => new FilterTypeBoolean(),
+    ], this));
     return this;
   }
   addString(name, label) {
     this._fields.push(new Field(name, label, 'string', [
-      () => FilterTypeString(),
-      () => FilterTypeBoolean(),
-    ]));
+      () => new FilterTypeString(),
+    ], this));
+    return this;
+  }
+  addNumber(name, label) {
+    this._fields.push(new Field(name, label, 'number', [
+      () => new FilterTypeBetween(),
+    ], this));
     return this;
   }
 }
